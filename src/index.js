e8f3c468517767f600860bb6e61bf5484631ace9
eliana osorio <yasminosorio2013@gmail.com>
2023-07-12 19:43:35 -0500

    feat: create function card validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
-console.log(validator);
+const getNumberCard = () => {
+
+    const numberCard = document.getElementById("cardCreditNumber").value;
+    const result = validator.isValid(numberCard);
+    const trueResponse = "Tarjeta Válida"
+    const falseResponse = "tarjeta No Válida";
+
+    //console.log(numberCard);
+
+    const checkValidate = document.getElementById("response");
+    checkValidate.innerHTML = result ? trueResponse : falseResponse;
+}
+const buttonCardValidate = document.getElementById("validator");
+buttonCardValidate.addEventListener("click", getNumberCard);
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
-};
+
+      isValid(number){
+
+        const cardArray = number.split("").reverse().join("");
+
+        let arrFilter = [];
+        let arrFilterImpar = [];
+
+        for (let i = 1; i < cardArray.length; i += 2) {
+            arrFilter.push(cardArray[i]);
+          }
+          for (let i = 0; i < cardArray.length; i += 2) {
+            arrFilterImpar.push(parseInt(cardArray[i]))
+          }
+
+        let arrFilterPair = [];
+        let counter = 0;
+        
+        for (let i = 0; i < arrFilter.length; i++) {
+            counter = arrFilter[i] * 2;
+            if (counter > 9) {
+
+              let secondDigit = counter % 10;
+              let firstDigit = parseInt(counter / 10);
+              let result = secondDigit + firstDigit;
+              arrFilterPair.push(result);
+          }else{
+            arrFilterPair.push(counter);
+          }
+        }
+
+
+        const arrNumbers = [...arrFilterImpar, ...arrFilterPair];
+        let sums = 0;
+        for (let i = 0; i < arrNumbers.length; i++) {
+            sums += arrNumbers[i];
+        }        
+          return sums % 10 === 0 ;
+      }
+    }
+
 
 export default validator;
